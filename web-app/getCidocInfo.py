d21a26c56c491c653603306a81b1da4b50fb5905
Josef Heers <josef.heers@gbv.de>
2025-02-05 16:48:12 +0100

    Namespaces

--- web-app/getCidocInfo.py
+++ web-app/getCidocInfo.py
@@ -3,27 +3,48 @@ import rdflib, json,sys
 from urllib.parse import urlparse
 
 
+NAMESPACES = [{'crm': 'http://www.cidoc-crm.org/cidoc-crm/'},
+              {'skos': 'http://www.w3.org/2004/02/skos/core#'},]
+
+def get_prefixes(graph):
+    prefixes = {}
+    for ns in NAMESPACES:
+        for k,v in ns.items():
+            prefixes[k] = v
+    for prefix, uri in graph.namespaces():
+        prefixes[prefix] = uri
+    return prefixes
+
+def prefix(qname,prefixes):
+    for k,v in prefixes.items():
+        if qname.startswith(v):
+            return qname.replace(v,k+':')
+    return qname
 
 def read_rdf_file(file_path):
     g = rdflib.Graph()
     g.parse(file_path)
     return g
 
-def split_url(url):
-    parts = urlparse(str(url))
-    vs = parts.path.split('/')
-    className = vs[-1]  
-    if parts.fragment:
-        className = parts.fragment
-    return parts.scheme+'://'+parts.netloc+'/'.join(vs[0:-1]),className
+def splitQname(qname):
+    if qname.startswith('http'):    
+        enitity=qname
+        ns = ''
+    else:
+        vx = qname.split(':')
+        if len(vx) == 2:
+            ns,enitity = vx
+        else:
+            ns,enitity = '',vx[-1]
+    return enitity,ns
 
 class Info():
-    def __init__(self,entity,prefix='crm'):
+    def __init__(self,entity,prefix=''):
         self.entity = entity
         self.prefix = prefix
 
-    def __lt__(self, value):
-        return self.entity < value.entity
+    def __lt__(self, other):
+        return self.entity < other.entity
     
     def __hash__(self):
         return hash(self.entity)
@@ -35,24 +56,24 @@ def findEntities(graph):
     isProp =rdflib.term.URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#Property')
     tP = rdflib.term.URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#type')
     isClass = rdflib.term.URIRef('http://www.w3.org/2000/01/rdf-schema#Class')
-    path = None
+    prefixes = get_prefixes(graph)
     for tS,tO in graph.subject_objects(tP):
-        fullPath,entity = split_url(tS)
-        if not path:
-            path = fullPath
+        qname = prefix(tS,prefixes) 
+        entity, ns = splitQname(qname)
+        info = Info(entity,ns)
         if tO == isProp:
-            propNames.add(Info(entity))
+            propNames.add(info)
         elif tO == isClass:
-            classNames.add(Info(entity))
+            classNames.add(info)
     data = {}
-    if path:
-        data['path'] = path
+    data['namespaces'] = prefixes
     if classNames:
         data['classes'] = [x.__dict__ for x in sorted(classNames)]
     if propNames:
         data['properties'] = [x.__dict__ for x in sorted(propNames)]
     return data
-    
+
+  
 if __name__ == "__main__":
     file_path = 'CIDOC_CRM_v7.1.1.rdf'
     args = sys.argv[1:]
