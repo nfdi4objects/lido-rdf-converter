55a15859fcf7a81f142dc3762de7154d3b7074c6
Josef Heers <josef.heers@gbv.de>
2026-03-11 13:56:27 +0000

    Refactor

--- pack_files.py
+++ pack_files.py
@@ -3,43 +3,38 @@ import zipfile
 import argparse
 from pathlib import Path
 
+def partition(lst, size):
+    for i in range(0, len(lst), size):
+        yield lst[i : i+size]
+
 def main():
     parser = argparse.ArgumentParser(description='Pack files from directory into N ZIP files')
-    parser.add_argument('-d', '--dir', default='rdfData', help='Directory to pack files from')
+    parser.add_argument('dir', help='Directory to pack files from')
     parser.add_argument('-n','--num', type=int, default=5, help='Number of ZIP files')
     parser.add_argument('-a','--archname', default='archive', help='Basename of ZIP files')
     args = parser.parse_args()
 
-    if not os.path.isdir(args.dir):
+    directory = Path(args.dir)
+    if not directory.is_dir():
         print("Directory does not exist")
         return
 
-    if args.num <= 0:
-        print("N must be positive")
+    num_zips = args.num
+    if num_zips <= 0:
+        print("#Zips must be positive")
         return
-
-    files = []
-    for root, dirs, filenames in os.walk(args.dir):
-        for filename in filenames:
-            files.append(os.path.join(root, filename))
-
-    if not files:
-        print("No files found")
-        return
-
-    # Split in N groups
-    groups = [[] for _ in range(args.num)]
-    for i, file in enumerate(files):
-        groups[i % args.num].append(file)
-
-    for i, group in enumerate(groups):
-        if not group:
+ 
+    # Get sorted files
+    files = sorted(directory.iterdir())
+    zip_size = 1 + len(files)//num_zips
+    # Make partitions and zip them
+    for i, part in enumerate(partition(files,zip_size),start=1):
+        if not part:
             continue
-        zip_name = f'{args.archname}_{i+1}.zip'
-        with zipfile.ZipFile(zip_name, 'w') as zipf:
-            for file in group:
-                #arcname = os.path.relpath(file, args.dir) # as archive fname
-                zipf.write(file)
+        zip_name = f'{args.archname}_{i}.zip'
+        with zipfile.ZipFile(zip_name, 'w') as zip_f:
+            for file in part:
+                zip_f.write(file)
         print(f'Created {zip_name}')
 
 if __name__ == '__main__':
